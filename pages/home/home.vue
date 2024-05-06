258d3d1944d98b64005884bf25c70540a2987a82
shanlihang <slh_ontuo@126.com>
2024-05-06 14:00:54 +0800

    first commit

---
+++ pages/home/home.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="home">
+		我的
+	</view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ pages/record/record.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="record">
+		记录
+	</view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped lang="less">
+
+</style>
